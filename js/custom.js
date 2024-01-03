8a49e30a201f8c28c2d7ec55b1b05220610e1b02
manggury <148739312+manggury@users.noreply.github.com>
2024-01-03 15:20:14 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,11 @@ $(function () {
     $('.visual_silde').slick({
         arrows: false,
         dots: true,
-        
+
     });
+
+    $('.header .material-symbols-outlined').on('click', function (e) {
+        e.preventDefault();
+        $('.header .gnb').toggleClass('on');
+    })
 });
